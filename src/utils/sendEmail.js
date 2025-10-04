31d1d14c0d6a1063e1ba701e377ac813c0dabde3
Asif <asifsaascraft@gmail.com>
2025-10-04 18:29:41 +0530

    Update email name heading

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -23,7 +23,7 @@ const sendEmailWithTemplate = async ({ to, name, templateKey, mergeInfo }) => {
       mail_template_key: templateKey,
       from: {
         address: process.env.ZEPTO_FROM,
-        name: "Hotel Booking Team", // change this to your brand
+        name: "SWE Pune 2025 : Hotel Booking Team", // change this to your brand
       },
       to: [
         {
